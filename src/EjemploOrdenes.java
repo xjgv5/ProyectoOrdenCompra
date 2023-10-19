ed2b5aacc45d5678faf7fddbe7f299c999a0f50b
Javier Gomez <javier.gov@outlook.es>
2023-10-18 20:08:00 -0600

    Modificacion a las clases

---
+++ src/EjemploOrdenes.java
@@ -0,0 +1,83 @@
+public class EjemploOrdenes {
+    OrdenCompra orden1 = new OrdenCompra("Compras componentes pc");
+        orden1.setCliente(new
+
+    Cliente("Andres","Guzman"));
+        orden1.setFecha(new
+
+    Date());
+
+        orden1.addProducto(new
+
+    Producto("Western Digital","Unidad SSD 480GB",54890));
+        orden1.addProducto(new
+
+    Producto("MSI","Monitor Optix G271",363940));
+        orden1.addProducto(new
+
+    Producto("AMD","Procesador AMD Ryzen 9 5900X",599990));
+        orden1.addProducto(new
+
+    Producto("EVGA","Tarjeta de video EVGA RTX 3080Ti",1849990));
+
+    OrdenCompra orden2 = new OrdenCompra("Compras Electrodomésticos");
+        orden2.setCliente(new
+
+    Cliente("John","Doe"));
+        orden2.setFecha(new
+
+    Date());
+        orden2.addProducto(new
+
+    Producto("Fensa","Cocina",239990));
+        orden2.addProducto(new
+
+    Producto("Samsung","Refrigerador",429990));
+        orden2.addProducto(new
+
+    Producto("Midea","Lavadora",149990));
+        orden2.addProducto(new
+
+    Producto("Aspiradora","IRobot",199990));
+
+    OrdenCompra orden3 = new OrdenCompra("Compras materiales de construcción");
+        orden3.setCliente(new
+
+    Cliente("Pepa","Roe"));
+        orden3.setFecha(new
+
+    Date());
+        orden3.addProducto(new
+
+    Producto("Melon","Cemento 25kg",4870));
+        orden3.addProducto(new
+
+    Producto("Grau","Hormigon",6040));
+        orden3.addProducto(new
+
+    Producto("Ceramicas Santiago","Ladrillo",790));
+        orden3.addProducto(new
+
+    Producto("Redline","Escala articulada aluminio",48990));
+
+    OrdenCompra[] ordenes = {orden1, orden2, orden3};
+
+        for(
+    OrdenCompra orden :ordenes)
+
+    {
+        System.out.println("Folio: " + orden.getIdentificador());
+        System.out.println("Cliente: " + orden.getCliente());
+        System.out.println("Descripcion: " + orden.getDescripcion());
+        System.out.println("Fecha: " + orden.getFecha());
+        System.out.println("Total: " + orden.getGranTotal());
+
+        int i = 1;
+        for (Producto p : orden.getProductos()) {
+            System.out.println("Producto " + i + ": " + p.getNombre() + " " + p.getFabricante() + " precio $" + p.getPrecio());
+            i++;
+        }
+    }
+}
+
+}
---
+++ src/OrdenCompra.java
@@ -0,0 +1,61 @@
+import java.util.Date;
+
+public class OrdenCompra {
+    private int identificador;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private Producto[] productos;
+
+    private static int ultimoID;
+    private int indiceProductos;
+
+    public OrdenCompra(String descripcion) {
+        this.descripcion = descripcion;
+        this.identificador = ++ultimoID;
+        this.productos = new Producto[4];
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Producto[] getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Producto[] productos) {
+        this.productos = productos;
+    }
+
+    public void addProducto(Producto producto){
+        if (indiceProductos < this.productos.length){
+            this.productos[indiceProductos++] = producto;
+        }
+    }
+
+    public int getTotal(){
+        int total = 0;
+        for (Producto p : productos){
+            total += p.getPrecio();
+        }
+        return total;
+    }
+}
